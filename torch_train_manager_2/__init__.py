a57c51a1013877d7f6ad8f67b439e61908e2e12a
Joaquin Zepeda <jazs.pro@gmail.com>
2024-01-02 10:59:32 -0800

    Adding training continuation solconf script

--- torch_train_manager_2/__init__.py
+++ torch_train_manager_2/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ["TrainManager", "always", "Extension"]
+__all__ = ["TrainManager", "always", "Extension", "init_from_checkpoint"]
 
 from .train_manager import TrainManager
 from .extensible import always
 from .extensions import Extension
+from .extensions.soleil import init_from_checkpoint
